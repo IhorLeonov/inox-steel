7e293848f2c5123fc4f5e70e3fe72017bf941a0e
Ihor Leonov <ihorleonow@gmail.com>
2025-02-26 14:34:06 +0200

    Update index.tsx

--- src/components/P/index.tsx
+++ src/components/P/index.tsx
@@ -7,7 +7,7 @@ export default function P({ children, className }: PProps) {
   return (
     <p
       className={cn(
-        'text-[14px] text-dark-grey md:text-[18px] xl:text-[20px]',
+        'text-[16px] text-dark-grey md:text-[18px] xl:text-[20px]',
         className,
       )}
     >
